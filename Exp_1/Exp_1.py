835c6ab083a6e20e2752ed94af17adcb5a2c01e0
bebin_k <46960666+SiriuXProtocoL@users.noreply.github.com>
2020-04-20 18:37:48 +0530

    Add files via upload

---
+++ Exp_1/Exp_1.py
@@ -0,0 +1,21 @@
+import time
+a = "Right Turn  >>>>>>"
+b = " Left Turn  <<<<<<"
+c = "Wheels Up ^^^^^^^"
+d = "Watch OOUUTTT ......@$%^@$%^@$%"
+e = "Crashed.......!!!!!"
+for i in range(20):
+	print(a)
+	time.sleep(0.2)
+for i in range(20):
+	print(b)
+	time.sleep(0.2)
+for i in range(20):
+	print(c)
+	time.sleep(0.2)
+for i in range(10):
+	print(d)
+	time.sleep(0.2)
+for i in range(50):
+	print(e)
+	time.sleep(0.1)
